0c348b5a204b67cdbf01d336c21f28eb3a421062
Andy <Orangebookair@gmail.com>
2022-04-15 17:54:48 -0500

    finishing touches for the April Update

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let oHeight = 0
 window.onload = (event) => { 
-    console.log('page is fully loaded');
+    //console.log('page is fully loaded');
 };
 window.onscroll = (event) => {
     //console.log("scrolling")
     //console.log(window.scrollY)
     //console.log(document.documentElement.scrollHeight)
     //console.log(window.innerHeight)
-    console.log(window.scrollY)
-    console.log((window.innerHeight))
-    console.log(document.documentElement.scrollHeight)
-    console.log(Number(window.scrollY)+Number(window.innerHeight))
+   // console.log(window.scrollY)
+    //console.log((window.innerHeight))
+    //console.log(document.documentElement.scrollHeight)
+    //console.log(Number(window.scrollY)+Number(window.innerHeight))
     if (Number(window.scrollY)+Number(window.innerHeight) == document.documentElement.scrollHeight){
         oHeight+=420
-        console.log("bottom");
+        //console.log("bottom");
         //console.log(eopclass);
         document.getElementById("eop").style.height=String(oHeight)+"px"
     }
