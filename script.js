cb166b0d48e4f6e6ee3583229eaffb6b4456a0c1
Andy <Orangebookair@gmail.com>
2022-04-15 17:10:13 -0500

    js attempt1

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+//let endOfPageDiv = document.getElementById("eop")
+let endOfPageDiv;
 window.onload = (event) => {
+    endOfPageDiv = document.getElementById("eop")
     console.log('page is fully loaded');
 };
+window.onscroll = (event) => {
+    //console.log("scrolling")
+    //console.log(window.scrollY)
+    //console.log(document.documentElement.scrollHeight)
+    //console.log(window.innerHeight)
+    console.log(window.scrollY)
+    console.log((window.innerHeight))
+    console.log(document.documentElement.scrollHeight)
+    console.log(Number(window.scrollY)+Number(window.innerHeight))
+    if (Number(window.scrollY)+Number(window.innerHeight) == document.documentElement.scrollHeight){
+        console.log("bottom")
+        console.log(endOfPageDiv)
+        console.log(endOfPageDiv.style.height);
+        //endOfPageDiv.style.height += 10;
+    }
+}
