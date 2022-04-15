d35021c897b45b2587495dddf896049a94d954af
Andy <Orangebookair@gmail.com>
2022-04-15 18:09:18 -0500

    420 buffer

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ window.onscroll = (event) => {
     //console.log((window.innerHeight))
     //console.log(document.documentElement.scrollHeight)
     //console.log(Number(window.scrollY)+Number(window.innerHeight))
-    if (Number(window.scrollY)+Number(window.innerHeight) >= document.documentElement.scrollHeight-69){
+    if (Number(window.scrollY)+Number(window.innerHeight) >= document.documentElement.scrollHeight-420){
         oHeight+=420
         //console.log("bottom");
         //console.log(eopclass);
