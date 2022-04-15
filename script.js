988da9558c7e1482d2a253923ff7e1248c2defc1
Andy <Orangebookair@gmail.com>
2022-04-15 17:24:39 -0500

    scrolling is working

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-//let endOfPageDiv = document.getElementById("eop")
-let endOfPageDiv;
-window.onload = (event) => {
-    endOfPageDiv = document.getElementById("eop")
+let oHeight = 0
+window.onload = (event) => { 
     console.log('page is fully loaded');
 };
 window.onscroll = (event) => {
@@ -14,9 +12,9 @@ window.onscroll = (event) => {
     console.log(document.documentElement.scrollHeight)
     console.log(Number(window.scrollY)+Number(window.innerHeight))
     if (Number(window.scrollY)+Number(window.innerHeight) == document.documentElement.scrollHeight){
-        console.log("bottom")
-        console.log(endOfPageDiv)
-        console.log(endOfPageDiv.style.height);
-        //endOfPageDiv.style.height += 10;
+        oHeight+=420
+        console.log("bottom");
+        //console.log(eopclass);
+        document.getElementById("eop").style.height=String(oHeight)+"px"
     }
 }
