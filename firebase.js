06c3fa2ea99906ef4f6bbbcec803f04a98e12ed4
Andy Phu <orangebookair@gmail.com>
2023-05-03 21:11:50 -0500

    firebase test

---
+++ firebase.js
@@ -0,0 +1,60 @@
+console.log("hello world")
+
+
+/*
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    // Your Firebase project configuration here
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // Get a reference to your Firebase Realtime Database
+  var database = firebase.database();
+  
+
+
+  // Retrieve data from Firebase Realtime Database
+database.ref('/caruseData/general').once('Andy', function(snapshot) {
+    // Get the data from the snapshot
+    var data = snapshot.val();
+  
+    // Loop through the data and print it to the console
+    for (var key in data) {
+      console.log(data[key]);
+    }
+  
+    // Display the retrieved data on your web page:
+    // Create an empty string to store the HTML for the data
+    var html = '';
+  
+    // Loop through the data and add it to the HTML string
+    for (var key in data) {
+      html += '<p>' + data[key] + '</p>';
+    }
+  
+    // Set the HTML of the <div> element to the data HTML string
+    document.getElementById('data').innerHTML = html;
+  });
+  
+
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9bbWR5g8qjPKOAhwvvb-wOsfzqlxOWPA",
+  authDomain: "caruse2-78cbf.firebaseapp.com",
+  projectId: "caruse2-78cbf",
+  storageBucket: "caruse2-78cbf.appspot.com",
+  messagingSenderId: "164646508888",
+  appId: "1:164646508888:web:5e552b9e966d6824db2d2d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+*/
