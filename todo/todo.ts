10f15c4b56aee532575d9fa4aaf398b77b574973
Andy Phu <orangebookair@gmail.com>
2023-05-08 21:09:21 -0500

    Mx

--- todo/todo.ts
+++ todo/todo.ts
@@ -1,10 +1,13 @@
-
+import { Links } from "./links.txt";
 //for every line in links.txt, create a new list element and append it to the list  
 document.addEventListener("DOMContentLoaded", () => {
 
 
     const listItem = document.createElement("li");
     listItem.textContent = "test";
-
-    document.getElementById("links-list").appendChild(listItem);
+    const linksList = document.getElementById("links-list")
+    if (linksList != null)
+        linksList.appendChild(listItem);
+    else
+        console.log("linksList is null"	);
 });
