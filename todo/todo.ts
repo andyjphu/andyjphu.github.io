9ab3f5b35ca9ff38452b48cbe96bdf7f8a7ee2e1
AndyP <malicioussoap@gmail.com>
2023-05-08 23:47:09 -0500

    fetch request test

--- todo/todo.ts
+++ todo/todo.ts
@@ -1,12 +1,19 @@
 //for every line in links.txt, create a new list element and append it to the list  
 document.addEventListener("DOMContentLoaded", () => {
-    const links = fetch("links.txt").then((response) => response.text());    
-    console.log(links);
-    const listItem = document.createElement("li");
-    listItem.textContent = "test";
-    const linksList = document.getElementById("links-list")
-    if (linksList != null)
-        linksList.appendChild(listItem);
-    else
-        console.log("linksList is null"	);
-});
+    fetch("todo/links.txt")
+      .then((response) => response.text())
+      .then((text) => {
+        const links = text.split("\n");
+        const linksList = document.getElementById("links-list");
+        if (linksList) {
+          links.forEach((link) => {
+            const listItem = document.createElement("li");
+            listItem.textContent = link;
+            linksList.appendChild(listItem);
+          });
+        } else {
+          console.log("linksList is null");
+        }
+      })
+      .catch((error) => console.log("Error fetching links:", error));
+  });
