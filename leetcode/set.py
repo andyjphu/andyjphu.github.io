674140d73e8a133f681effd339744640f80c59a0
AndyP <malicioussoap@gmail.com>
2023-05-24 11:13:58 -0500

    heapify

---
+++ leetcode/set.py
@@ -0,0 +1,14 @@
+b = set()
+b.add("HII")
+b.add("HII")
+b.add("HII")
+b.add("HII")
+b.add((1,0))
+print("HII" in b)
+print(tuple([1,0]) in b)
+
+if True:
+    ...
+    print("HI")
+else:
+    print("beans")
